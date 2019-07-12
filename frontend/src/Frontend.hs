5b38dfc2f44ae41addc63e4bc12b43a1b0ce3e37
Ryan Trinkle <ryan.trinkle@gmail.com>
2019-07-12 15:38:23 -0400

    Add immediate instantiation example

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -7,6 +7,8 @@ import qualified Data.Text as T
 import Obelisk.Frontend
 import Obelisk.Route
 import Reflex.Dom.Core
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 import Common.Api
 import Common.Route
@@ -17,7 +19,17 @@ frontend :: Frontend (R FrontendRoute)
 frontend = Frontend
   { _frontend_head = el "title" $ text "Obelisk Minimal Example"
   , _frontend_body = do
-      text "Welcome to Obelisk!"
-      el "p" $ text $ T.pack commonStuff
-      elAttr "img" ("src" =: static @"obelisk.jpg") blank
+      i <- inputElement def
+      b <- button "Create accumulator"
+      let createAccumulator = current (value i) <@ b
+          f _ = foldDyn (:) [] createAccumulator
+          accumulatorCreated = pushAlways f createAccumulator
+          insertAbove :: a -> Map Int a -> Map Int a
+          insertAbove v m = Map.insert k v m
+            where k = case Map.maxViewWithKey m of
+                    Nothing -> 1
+                    Just ((oldK, _), _) -> succ oldK
+      accumulators <- foldDyn insertAbove mempty accumulatorCreated
+      display $ joinDynThroughMap accumulators
+      pure ()
   }
